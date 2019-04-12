63779b88b65d7e4402fc7b8fe998494bf92be7c6
Katrine-Marie <fjordscene@gmail.com>
2019-04-12 11:57:04 +0200

    Created basic views for default controller

---
+++ public/views/pages/about.php
@@ -0,0 +1,7 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1>
+	About Page
+</h1>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
---
+++ public/views/pages/index.php
@@ -0,0 +1,7 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1>
+	<?php echo $data['title']; ?>
+</h1>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
