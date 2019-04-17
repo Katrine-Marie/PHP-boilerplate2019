440fd7342e5717a99dd4fbb9ae12403df468a394
Katrine-Marie <fjordscene@gmail.com>
2019-04-17 10:25:44 +0200

    Added login check methods

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -5,7 +5,38 @@
       $this->userModel = $this->model('User');
     }
 
+    public function index(){
+      redirect('welcome');
+    }
 
 
+
+
+
+    // Create Session With User Info
+    public function createUserSession($user){
+      $_SESSION['user_id'] = $user->id;
+      $_SESSION['user_email'] = $user->email;
+      $_SESSION['user_name'] = $user->name;
+      redirect('pages/index');
+    }
+
+    // Logout & Destroy Session
+    public function logout(){
+      unset($_SESSION['user_id']);
+      unset($_SESSION['user_email']);
+      unset($_SESSION['user_name']);
+      session_destroy();
+      redirect('pages/index');
+    }
+
+    // Check Logged In
+    public function isLoggedIn(){
+      if(isset($_SESSION['user_id'])){
+        return true;
+      } else {
+        return false;
+      }
+    }
 
   }
