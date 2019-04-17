c213876497e8466ec22540ff7aaf6044b90f1442
Katrine-Marie <fjordscene@gmail.com>
2019-04-17 10:27:58 +0200

    Created login method

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -12,6 +12,79 @@
 
 
 
+    public function login(){
+      // Check if logged in
+      if($this->isLoggedIn()){
+        redirect('posts');
+      }
+
+      // Check if POST
+      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // Sanitize POST
+        $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        $data = [
+          'email' => trim($_POST['email']),
+          'password' => trim($_POST['password']),
+          'email_err' => '',
+          'password_err' => '',
+        ];
+
+        // Check for email
+        if(empty($data['email'])){
+          $data['email_err'] = 'Please enter email.';
+        }
+
+        // Check for name
+        if(empty($data['name'])){
+          $data['name_err'] = 'Please enter name.';
+        }
+
+        // Check for user
+        if($this->userModel->findUserByEmail($data['email'])){
+          // User Found
+        } else {
+          // No User
+          $data['email_err'] = 'This email is not registered.';
+        }
+
+        // Make sure errors are empty
+        if(empty($data['email_err']) && empty($data['password_err'])){
+
+          // Check and set logged in user
+          $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+
+          if($loggedInUser){
+            // User Authenticated!
+            $this->createUserSession($loggedInUser);
+
+          } else {
+            $data['password_err'] = 'Password incorrect.';
+            // Load View
+            $this->view('users/login', $data);
+          }
+
+        } else {
+          // Load View
+          $this->view('users/login', $data);
+        }
+
+      } else {
+        // If NOT a POST
+
+        // Init data
+        $data = [
+          'email' => '',
+          'password' => '',
+          'email_err' => '',
+          'password_err' => '',
+        ];
+
+        // Load View
+        $this->view('users/login', $data);
+      }
+    }
+
 
     // Create Session With User Info
     public function createUserSession($user){
