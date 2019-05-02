dd9c3e8d044b4e12b3b32713a6f7cf6d4eda7d75
Katrine-Marie <fjordscene@gmail.com>
2019-05-02 14:34:26 +0200

    Added backup PWA support to header

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,6 +6,21 @@
 
 	<link href="<?php echo URLROOT; ?>/manifest.json" rel="manifest">
 
+	<!-- Backup PWA support -->
+	<meta name="theme-color" content="#aaa">
+	<meta name="apple-mobile-web-app-status-bar-style" content="#aaa">
+
+	<meta name="apple-mobile-web-app-capable" content="yes"> <!-- iOS -->
+  <meta name="mobile-web-app-capable" content="yes"> <!-- Android -->
+
+	<!-- Favicon and backup touch icon support -->
+	<link rel="icon" type="image/png" href="<?php echo URLROOT; ?>/img/icons/icon_36.png">
+
+	<link rel="icon" sizes="192x192" href="<?php echo URLROOT; ?>/img/icons/icon_192.png">
+	<link rel="icon" sizes="144x144" href="<?php echo URLROOT; ?>/img/icons/icon_144.png">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo URLROOT; ?>/img/icons/icon_144.png">
+  <link rel="apple-touch-icon-precomposed" href="<?php echo URLROOT; ?>/img/icons/icon_144.png">
+
 	<title><?php echo SITENAME; ?></title>
 
 	<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/minified/style.css">
