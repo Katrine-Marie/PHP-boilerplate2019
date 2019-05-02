e3a1a89b0e806d4f56a18ad78a3726bf3fb0ef5c
Katrine-Marie <fjordscene@gmail.com>
2019-05-02 14:38:48 +0200

    Added social media and SEO meta tags

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<title><?php echo SITENAME; ?></title>
+
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -21,7 +23,30 @@
   <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo URLROOT; ?>/img/icons/icon_144.png">
   <link rel="apple-touch-icon-precomposed" href="<?php echo URLROOT; ?>/img/icons/icon_144.png">
 
-	<title><?php echo SITENAME; ?></title>
+	<!--  SEO -->
+	<meta name="description" content="<?php echo SITENAME; ?>">
+	<meta name="keywords" content="">
+	<meta name="robots" content="index,follow,noodp">
+
+	<meta itemprop="name" content="<?php echo SITENAME; ?>">
+	<meta itemprop="description" content="">
+	<meta itemprop="image" content="">
+
+	<!-- Social media -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@">
+	<meta name="twitter:title" content="<?php echo SITENAME; ?>">
+	<meta name="twitter:description" content="">
+	<meta name="twitter:creator" content="@">
+	<meta name="twitter:image:src" content="">
+
+	<meta property="og:title" content="<?php echo SITENAME; ?>" />
+	<meta property="og:url" content="<?php echo URLROOT; ?>" />
+	<meta property="og:image" content="" />
+	<meta property="og:description" content="" />
+	<meta property="og:site_name" content="<?php echo SITENAME; ?>" />
+	<meta property="og:locale" content="en_UK" />
+
 
 	<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/minified/style.css">
 </head>
